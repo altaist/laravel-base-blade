5e07f2284fc1ca26343e0ce6c24bc98131c6ffa7
Altaist <apechersky@ya.ru>
2025-09-01 18:06:31 +0700

    init blade layout

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,9 @@
+<div class="container py-5">
+    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Добро пожаловать в Kvadro!</h1>
+        <p class="col-md-8 fs-4">Здесь вы найдете лучшие квадроциклы и аксессуары.</p>
+        <button class="btn btn-primary btn-lg" type="button">Узнать больше</button>
+      </div>
+    </div>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('header')
+    @include('components.header')
+@endsection
+
+@section('content')
+    @include('components.hero')
+@endsection
