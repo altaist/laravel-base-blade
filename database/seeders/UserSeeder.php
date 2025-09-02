965f238df8d4dce596b42d3e8fba96f93fc99012
Altaist <apechersky@ya.ru>
2025-09-02 10:40:47 +0700

    fix user seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,15 +5,19 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Enums\UserRole;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
         // Create admin user
+        $password = Hash::make('12345678');
+
         User::factory()->create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
+            'password' => $password,
             'role' => UserRole::ADMIN,
         ]);
 
@@ -21,13 +25,15 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'Manager User',
             'email' => 'manager@example.com',
+            'password' => $password,
             'role' => UserRole::MANAGER,
         ]);
 
         // Create regular users
-        User::factory(3)->create([
+        User::factory()->create([
             'name' => 'User',
             'email' => 'user@example.com',
+            'password' => $password,
             'role' => UserRole::USER,
         ]);
     }
