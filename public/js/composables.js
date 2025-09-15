180202562fbf222784ca1a41e6dbc5b6d3715a63
Altaist <apechersky@ya.ru>
2025-09-15 15:31:30 +0700

    add composables

---
+++ public/js/composables.js
@@ -0,0 +1,8 @@
+/**
+ * Composables - общие функции use для проекта
+ */
+
+// Пустой файл для будущих composables функций
+// Здесь будут общие функции типа useAuth, useApi, useModal и т.д.
+
+console.log('Composables загружены');
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,7 @@
     <x-layout.footer />
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/composables.js') }}"></script>
     <script src="{{ asset('js/image-preview.js') }}"></script>
     @if(request()->routeIs('home') || request()->routeIs('article.show'))
     @endif
